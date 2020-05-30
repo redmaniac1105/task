a46dcb77ff36d92cd94bf5bdf8651d5c759eec04
redmaniac1105 <59756821+redmaniac1105@users.noreply.github.com>
2020-05-30 20:19:07 +0530

    Update random_forest_regression.py

--- random_forest_regression.py
+++ random_forest_regression.py
@@ -36,13 +36,3 @@ regressor.fit(X_train, y_train)
 
 # Predicting a new result
 y_pred=regressor.predict(X_test)
-
-# Visualising the Random Forest Regression results (higher resolution)
-X_grid = np.arange(min(X), max(X), 0.01)
-X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Random Forest Regression)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
-plt.show()
